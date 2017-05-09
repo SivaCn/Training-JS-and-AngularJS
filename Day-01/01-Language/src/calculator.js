eb490a1605517464a7373a3d044eb2fa5eb7d0b6
Magesh <tkmagesh77@gmail.com>
2017-05-09 16:54:38 +0530

    Day-01 Complete

--- Day-01/01-Language/src/calculator.js
+++ Day-01/01-Language/src/calculator.js
@@ -1,3 +1,46 @@
 function add(x,y){
-	return x + y;
+	function parseArg(n){
+		if (Array.isArray(n)) return add.apply(this, n);
+		if (typeof n === 'function') return parseArg(n());
+		return isNaN(n) ? 0 : parseInt(n,10);
+	}
+	return arguments.length <= 1 ? parseArg(arguments[0]) : parseArg(arguments[0]) + add([].slice.call(arguments, 1));
 }
+
+//As a method of an obj
+//	this -> obj
+
+//As a function
+//	this -> global scope (window)
+
+//Using the call() of the function
+
+//Using the apply() of the function
+
+/*sum(10,20,30,40)
+	return 10 + sum(20,30,40)
+			return 20 + sum(30,40)
+					return 30 + sum(40)
+							return 40*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
